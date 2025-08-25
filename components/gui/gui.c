41aaa5d0dbae152eba230db4d1669ce837fa7f25
novaeco <novatribolnovea@gmail.com>
2025-08-26 00:48:46 +0200

    refactor: migrate GUI to LVGL v9 API

--- components/gui/gui.c
+++ components/gui/gui.c
@@ -6,16 +6,16 @@
 #include "config.h"
 
 static esp_lcd_panel_handle_t s_panel;
-static lv_disp_draw_buf_t s_draw_buf;
-static lv_color_t *s_buf1;
+static lv_draw_buf_t *s_draw_buf;
+static uint8_t *s_buf1;
 
-static void lvgl_flush_cb(lv_disp_drv_t *drv, const lv_area_t *area, lv_color_t *color_p)
+static void lvgl_flush_cb(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map)
 {
-    esp_lcd_panel_draw_bitmap(s_panel, area->x1, area->y1, area->x2 + 1, area->y2 + 1, color_p);
-    lv_disp_flush_ready(drv);
+    esp_lcd_panel_draw_bitmap(s_panel, area->x1, area->y1, area->x2 + 1, area->y2 + 1, px_map);
+    lv_display_flush_ready(disp);
 }
 
-static void lvgl_touch_read(lv_indev_drv_t *drv, lv_indev_data_t *data)
+static void lvgl_touch_read(lv_indev_t *indev, lv_indev_data_t *data)
 {
     touch_gt911_point_t p = touch_gt911_read_point(1);
     if (p.cnt > 0) {
@@ -41,22 +41,15 @@ void gui_init(esp_lcd_panel_handle_t panel)
     s_panel = panel;
     lv_init();
 
-    s_buf1 = heap_caps_malloc(LCD_H_RES * 10 * sizeof(lv_color_t), MALLOC_CAP_DMA | MALLOC_CAP_SPIRAM);
-    lv_disp_draw_buf_init(&s_draw_buf, s_buf1, NULL, LCD_H_RES * 10);
+    s_draw_buf = lv_draw_buf_create(LCD_H_RES, 10, LV_COLOR_FORMAT_NATIVE, LV_STRIDE_AUTO);
+    s_buf1 = s_draw_buf->data;
 
-    lv_disp_drv_t disp_drv;
-    lv_disp_drv_init(&disp_drv);
-    disp_drv.hor_res = g_display.width;
-    disp_drv.ver_res = g_display.height;
-    disp_drv.flush_cb = lvgl_flush_cb;
-    disp_drv.draw_buf = &s_draw_buf;
-    lv_disp_drv_register(&disp_drv);
+    lv_display_t *disp = lv_display_create(g_display.width, g_display.height);
+    lv_display_set_flush_cb(disp, lvgl_flush_cb);
+    lv_display_set_buffers(disp, s_buf1, NULL, LCD_H_RES * 10, LV_DISPLAY_RENDER_MODE_PARTIAL);
 
-    lv_indev_drv_t indev_drv;
-    lv_indev_drv_init(&indev_drv);
-    indev_drv.type = LV_INDEV_TYPE_POINTER;
-    indev_drv.read_cb = lvgl_touch_read;
-    lv_indev_drv_register(&indev_drv);
+    lv_indev_t *indev = lv_indev_create();
+    lv_indev_set_read_cb(indev, lvgl_touch_read);
 
     xTaskCreate(lvgl_task, "lvgl", 4096, NULL, 5, NULL);
 }
